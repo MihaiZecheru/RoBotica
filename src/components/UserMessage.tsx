e0fe66667c1ccc3b8a6acf3db8a1f618847e8399
MihaiZecheru <zecheruchris@gmail.com>
2024-11-09 09:56:11 -0800

    Disable highlighting

--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -9,7 +9,7 @@ const UserMessage = ({ content, avatar_url }: Props) => {
   return (
     <Paper className="user-message" elevation={2} sx={{ borderRadius: '1rem', padding: '.5rem', marginBottom: '1rem' }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-        <span style={{ margin: '.25rem', marginLeft: '.5rem', fontWeight: 900 }} className="blue-highlight-color">{content}</span>
+        <span style={{ margin: '.25rem', marginLeft: '.5rem', fontWeight: 900, userSelect: 'none' }}>{content}</span>
         <div style={{ width: '40px', height: '40px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <Avatar
             className="chat-avatar"
