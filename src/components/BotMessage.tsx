e61bc555dc7f015c212cbf9f4189ee11e1b9b6aa
MihaiZecheru <zecheruchris@gmail.com>
2024-11-09 23:36:10 -0800

    Translate message on avatar click

--- src/components/BotMessage.tsx
+++ src/components/BotMessage.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Paper } from "@mui/material";
-import '../styles/blue-highlight-color.css';
+import { Avatar, Paper, Tooltip } from "@mui/material";
 import ClickableWord from "./ClickableWord";
 import TLanguage from "../database/TLanguage";
+import useInfoModal from "./base/useInfoModal";
+import Bot from "../functions/Bot";
 
 interface Props {
   content: string;
@@ -9,11 +10,29 @@ interface Props {
 }
 
 const BotMessage = ({ content, language }: Props) => {
+  const showInfoModal = useInfoModal();
+
+  /**
+   * Translate the entire bot's message. To be used on the Avatar's onClick event.
+   */
+  const translateBotMessageOnAvatarClick = async () => {
+    const translation = await Bot.GenerateMessageTranslation(content, language);
+    showInfoModal(`${language} Message Translation`, `${content}\n\n${translation}`);
+  };
+
   return (
     <Paper className="bot-message" elevation={2} sx={{ borderRadius: '1rem', padding: '.5rem', marginBottom: '1rem' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar className="chat-avatar" alt='pfp' src='./robotica.png' />
-        <div style={{ margin: '.25rem', fontWeight: 900 }} className="blue-highlight-color">
+        <Tooltip title="Translate message" placement="top-start">
+          <Avatar
+            className="chat-avatar"
+            alt='pfp'
+            src='./robotica.png'
+            onClick={translateBotMessageOnAvatarClick}
+            sx={{ cursor: 'pointer' }}
+          />
+        </Tooltip>
+        <div style={{ margin: '.25rem', fontWeight: 900 }}>
           {
             content.split(' ').map((word: string, index: number) => 
               <ClickableWord key={index} word={word} language={language} />
