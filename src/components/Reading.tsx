60a4a118f52eabc90fa1c86ac3604b59a0d51c37
MihaiZecheru <zecheruchris@gmail.com>
2024-11-11 01:25:36 -0800

    Add navily in bottom right corner

--- src/components/Reading.tsx
+++ src/components/Reading.tsx
@@ -1,16 +1,18 @@
-import { Paper } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 import { useState } from 'react';
 import StoriesSearch from './StoriesSearch';
 import StoryView from './StoryView';
 import TStory from '../database/TStory';
 import TLanguage from '../database/TLanguage';
 import '../styles/reading-page.css';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   language: TLanguage;
 }
 
 const ReadingPage = ({ language }: Props) => {
+  const navigate = useNavigate();
   const [story, setStory] = useState<TStory | null>(null);
 
   const storySelected = (story: TStory) => {
@@ -30,6 +32,9 @@ const ReadingPage = ({ language }: Props) => {
           : <StoryView language={language} title={story.title} body={story.body} backToSearch={backToSearch} />
         }
       </Paper>
+      <div style={{ position: 'fixed', bottom: '.25rem', right: '.75rem' }}>
+        <Button color="primary" onClick={() => navigate('/navily')}>Navily</Button>
+      </div>
     </div>
   );
 }
