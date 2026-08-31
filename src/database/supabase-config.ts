11ec478b090c52759a82aa2b9f5ecb6a273b3bf8
Mihai Zecheru <zecheruchris@gmail.com>
2026-08-31 12:47:31 -0700

    Fix lock error

--- src/database/supabase-config.ts
+++ src/database/supabase-config.ts
@@ -3,5 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    lock: async (_name: string, _acquireTimeout: number, fn: () => Promise<any>) => {
+      return await fn();
+    },
+  },
+});
+
 export default supabase;
